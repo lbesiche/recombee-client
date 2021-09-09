a6a493a68a825d4f6a30116f720ab84fe1d1b737
Jose Besiche <josebesiche@joses-air.attlocal.net>
2021-09-08 22:33:14 -0500

    added recommend_items_to_users function to package

--- lib/recombee_client.dart
+++ lib/recombee_client.dart
@@ -8,6 +8,7 @@ export 'src/recombee_client_base.dart';
 export 'src/requests/add_cart_addition.dart';
 export 'src/requests/add_detail_view.dart';
 export 'src/requests/recombee_request.dart';
+export './src/requests/recommend_items_to_users.dart';
 export 'src/exceptions/recombee_response_exception.dart';
 
 export 'src/options/recommend_items_to_users_options.dart';
